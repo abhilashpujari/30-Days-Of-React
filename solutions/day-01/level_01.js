aa2fe3409f92c97ec5e9ea933d2656811dfed4f7
abhilash <abhilashpujari11@gmail.com>
2022-10-30 11:57:54 +0530

    solution for day 1

---
+++ solutions/day-01/level_01.js
@@ -0,0 +1,6 @@
+let numArr = [];
+let numArr1 = Array(6).fill(0);
+console.log('length of numArr1 ', numArr1.length);
+console.log('numArr1 first ', numArr1[0]);
+console.log('numArr1 middle ', numArr1[numArr1.length / 2]);
+console.log('numArr1 last ', numArr1[numArr1.length - 1]);
